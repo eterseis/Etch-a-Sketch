5e77bdd6b838fed9f59d5147e00a1b7e6c013c4c
Eterseis <148352159+eterseis@users.noreply.github.com>
2025-01-06 14:42:02 -0300

    Rework 'style.css' and 'scrip.js'
    remade whole grid logic and ui.

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,19 @@
 let container = document.querySelector(".container");
 
-for (let i = 0; i < 50; i++) {
-  for (let j = 0; j < 50; j++) {
-    container.appendChild(document.createElement("div"));
+let n = 16;
+for (let i = 0; i < n; i++){
+  let row = document.createElement("div");
+  row.setAttribute("class", "row");
+  container.appendChild(row);
+  for (let j = 0; j < n; j++){
+    let column = document.createElement("div");
+    column.setAttribute("class", "column");
+    row.appendChild(column);
   }
 }
 
 container.addEventListener("mouseover", (e) => {
-  if (e.target.parentElement.className == "container") {
+  if (e.target.className == "column") {
     e.target.style.backgroundColor = `rgb(${getRandomInt()}, 40, ${getRandomInt()})`;
   }
 });
