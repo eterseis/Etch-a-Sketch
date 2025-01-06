4115440aff84f71b3aad6cc9952adf8e76a5ae28
Eterseis <148352159+eterseis@users.noreply.github.com>
2025-01-06 18:13:14 -0300

    Added a 'button' so as to make a new grid and more
    wrote all the code to reset the old grid and handle validation.
    new 'SetGrid' function --i don't have to explain it

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,31 @@
 let container = document.querySelector(".container");
 
-let n = 16;
-for (let i = 0; i < n; i++){
-  let row = document.createElement("div");
-  row.setAttribute("class", "row");
-  container.appendChild(row);
-  for (let j = 0; j < n; j++){
-    let column = document.createElement("div");
-    column.setAttribute("class", "column");
-    row.appendChild(column);
+function SetGrid(userInput) {
+  for (let i = 0; i < userInput; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    container.appendChild(row);
+    for (let j = 0; j < userInput; j++) {
+      let column = document.createElement("div");
+      column.setAttribute("class", "column");
+      row.appendChild(column);
+    }
   }
 }
+
+let btn = document.querySelector(".newGrid");
+btn.addEventListener("click", () => {
+  let userInput = window.prompt("enter a new grid between 1-100: ");
+  if (!(userInput > 0 && userInput < 101)) {
+    window.alert("error: value not in range");
+  } else {
+    let rows = document.querySelectorAll(".row");
+    rows.forEach((e) => {
+      container.removeChild(e);
+    });
+    SetGrid(userInput);
+  }
+});
 
 container.addEventListener("mouseover", (e) => {
   if (e.target.className == "column") {
