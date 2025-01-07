7817c77ef3dd8d3c20295f5e8b1314281f1936d4
Eterseis <148352159+eterseis@users.noreply.github.com>
2025-01-06 22:25:40 -0300

    Added progressive darkening effect
    added some effects to 'btn-newGrid'
    the grid starts with 16x16 for now

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,5 @@
 let container = document.querySelector(".container");
+SetGrid(16);
 
 function SetGrid(userInput) {
   for (let i = 0; i < userInput; i++) {
@@ -13,7 +14,7 @@ function SetGrid(userInput) {
   }
 }
 
-let btn = document.querySelector(".newGrid");
+let btn = document.querySelector(".btn-newGrid");
 btn.addEventListener("click", () => {
   let userInput = window.prompt("enter a new grid between 1-100: ");
   if (!(userInput > 0 && userInput < 101)) {
@@ -28,8 +29,15 @@ btn.addEventListener("click", () => {
 });
 
 container.addEventListener("mouseover", (e) => {
-  if (e.target.className == "column") {
-    e.target.style.backgroundColor = `rgb(${getRandomInt()}, 40, ${getRandomInt()})`;
+  let target = e.target;
+  if (target.className == "column") {
+    if (target.style.backgroundColor == ""){
+      target.style.backgroundColor = `rgb(${getRandomInt()}, 40, ${getRandomInt()})`;
+      target.style.opacity = 0.6;
+    }
+    else{
+      target.style.opacity *= 1.4;
+    }
   }
 });
 
